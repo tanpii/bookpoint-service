f9eb12f3d1ffe456859d1aa4533f267ea5ba8eff
Tatyana Porseva <tporseva@inbox.ru>
2025-05-04 23:34:03 +0400

    remove generated and IDE files from version control

--- build/scripts/protoc-gen-grpc-kotlin-1.4.1-jdk8-generateProto-trampoline.bat
+++
@@ -1,2 +0,0 @@
-@ECHO OFF
-"C:\Program Files\jdk-21.0.1\bin\java.exe" -jar "C:\Users\liljarn\.gradle\caches\modules-2\files-2.1\io.grpc\protoc-gen-grpc-kotlin\1.4.1\4ce0f649481b13711e8d70da5253028d36b087ba\protoc-gen-grpc-kotlin-1.4.1-jdk8.jar" %*
